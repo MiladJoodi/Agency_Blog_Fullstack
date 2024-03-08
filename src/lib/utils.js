406e0e426780084c0c6859561b71fa9084b7c2dc
Milad Joodi <winbeta.com@gmail.com>
2024-03-08 17:23:34 +0330

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,17 @@
 const { default: mongoose } = require("mongoose");
 
-const connectToDb = async ()=>{
+const connection = {};
+
+export const connectToDb = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO);
+        if(connection.isConnected){
+            console.log("Using existing connection")
+            return;
+        }
+        const db = await mongoose.connect(process.env.MONGO);
+        connection.isConnected = db.connections[0].readyState;
       } catch (error) {
         console.log(error)
-        throw new Error("Error connecting to database")
+        throw new Error(error)
       }
 }
