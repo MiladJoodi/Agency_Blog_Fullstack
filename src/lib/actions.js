6b671c0b568b555f56187a33edbb2bc910d8c61b
Milad Joodi <winbeta.com@gmail.com>
2024-03-09 12:50:03 +0330

    serave

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,8 +1,12 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
 import { Post } from "./models";
 import { connectToDb } from "./utils";
 
+
+// Add Post
 export const addPost = async (formData) => {
-    "use server"
 
     const { title, desc, slug, userId } = Object.fromEntries(formData);
 
@@ -13,8 +17,26 @@ export const addPost = async (formData) => {
             desc,
             slug,
             userId
-        })
+        });
 
+        await newPost.save()
+        console.log("save to db")
+        revalidatePath("/blog")
+    } catch (err) {
+        console.log(err)
+        return {error: "Something went wrong!"}
+    }
+}
+
+//Delete
+export const deletePost = async (formData) => {
+    const { id } = Object.fromEntries(formData);
+    try {
+        connectToDb()
+
+        await Post.findByIdAndDelete(id);
+        console.log("Deleted from db")
+        revalidatePath("/blog")
     } catch (err) {
         console.log(err)
         return {error: "Something went wrong!"}
