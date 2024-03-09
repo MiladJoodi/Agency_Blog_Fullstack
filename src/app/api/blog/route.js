9939aa11ecc96c9009808cdb261a357aee64a3fd
Milad Joodi <winbeta.com@gmail.com>
2024-03-09 13:50:05 +0330

    0

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,10 +1,14 @@
+import { Post } from "@/lib/models"
 import { connectToDb } from "@/lib/utils"
+import { NextResponse } from "next/server"
 
-export const Get = async ()=>{
+export const Get = async (request)=>{
     try{
         connectToDb()
 
-        
+        const posts = await Post.find();
+        return NextResponse.json(posts);
+
     }catch(err){
         console.log(err)
         throw new Error("Failed to fetch posts!")
