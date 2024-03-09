b5e11ebd0cb888ad285dcad1898939e99e257938
Milad Joodi <winbeta.com@gmail.com>
2024-03-09 12:25:40 +0330

    update today

---
+++ src/lib/actions.js
@@ -0,0 +1,22 @@
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+    "use server"
+
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try {
+        connectToDb()
+        const newPost = new Post({
+            title,
+            desc,
+            slug,
+            userId
+        })
+
+    } catch (err) {
+        console.log(err)
+        return {error: "Something went wrong!"}
+    }
+}
